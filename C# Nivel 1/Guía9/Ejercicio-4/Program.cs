3fad1338402b13de951bef71898a348d8acf9952
Pablo Diosquez <pablodiosquez24@gmail.com>
2023-02-08 15:47:48 -0300

    Actualización de la Guía 9

---
+++ C# Nivel 1/Guía9/Ejercicio-4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ejercicio_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Diseñar una función que invierta una cadena de caracteres de una palabra. La
+            // cadena debe estar validada para que contenga una sola palabra.  
+
+
+            Console.WriteLine(invertir("hola como estas"));
+
+        }
+
+        public static string invertir(string cadena)
+        {
+            string res = "";
+
+            for (int i = cadena.Length - 1; i > -1; i--)
+            {
+                res += cadena[i];
+            }
+
+            return res;
+        }
+    }
+}
---
+++ C# Nivel 1/Guía9/Ejercicio-5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Ejercicio_3;
+
+namespace Ejercicio_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //     Diseñar una función que devuelva la cantidad de veces que un carácter dado
+        //     está contenido en una palabra también dada. La cadena de caracteres debe
+        //     estar validada para que contenga sólo una palabra.
+
+            Console.WriteLine("Palabra:");
+            string palabra = Console.ReadLine();
+
+            if (Ejercicio_3.Program.validarCadena(palabra))
+            {
+                Console.WriteLine("Caracter:");
+                char caracter = Console.ReadLine();
+
+                Console.WriteLine($"La cantidad de veces que {caracter} aparece en {palabra} es: {apariciones(palabra, caracter)}");
+            }
+            else
+            {
+                Console.WriteLine("La cadena debe contener una sola palabra 😕");
+            }
+
+        }
+
+        public static int apariciones(string palabra, char carac)
+        {
+            int cant = 0;
+
+            foreach (char letra in palabra)
+            {
+                if (letra == carac)
+                {
+                    cant++;
+                }
+            }
+
+            return cant;
+        }
+    }
+}
